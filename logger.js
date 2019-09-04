02d6c598706a865f17501f7c0a6c2e7de9a0f0d7
a <alexanderddking@gmail.com>
2019-09-05 08:02:26 +0900

    Remove all build files

--- logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function createLogger(name) {
-    return function ({}) {
-        console.log(`🏞️ [@${name}]`, ...arguments);
-    };
-}
-exports.createLogger = createLogger;
-//# sourceMappingURL=logger.js.map
--- utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongodb_1 = require("mongodb");
-function startMongoConfig(MONGO_USER, MONGO_PASSWORD) {
-    const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0-g5hyn.gcp.mongodb.net/test?retryWrites=true&w=majority`;
-    return new mongodb_1.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-}
-exports.startMongoConfig = startMongoConfig;
-//# sourceMappingURL=utils.js.map
